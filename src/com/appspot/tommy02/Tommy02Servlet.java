bdc4e93fd6e38b2fc0b62701be3749e14ba919f5
tommy <tommy@192.168.10.107>
2015-11-29 23:16:21 +0900

    test

--- src/com/appspot/tommy02/Tommy02Servlet.java
+++ src/com/appspot/tommy02/Tommy02Servlet.java
@@ -1,7 +1,12 @@
 package com.appspot.tommy02;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// commit test
 
 @SuppressWarnings("serial")
 public class Tommy02Servlet extends HttpServlet {
