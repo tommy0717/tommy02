97ca7b4eadd0c69b23967e14bd1124a3219f209e
tommy <tommy@192.168.10.103>
2015-12-23 14:01:01 +0900

    自動ログインの追加

--- src/com/appspot/tommy02/IndexServlet.java
+++ src/com/appspot/tommy02/IndexServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @SuppressWarnings("serial")
 public class IndexServlet extends HttpServlet {
@@ -26,10 +28,50 @@ public class IndexServlet extends HttpServlet {
 				break;
 
 			case "login":
-				/*/WEB-INF/login.jspを呼び出す */
-				RequestDispatcher rd1 = getServletContext().getRequestDispatcher("/WEB-INF/login.jsp");
-				rd1.forward(req, resp);
-				break;
+				//自動ログイン確認
+				//必要情報を取得する（リクエスト＆セッション＆クッキー）
+				HttpSession session = req.getSession();
+				Cookie ck[] = req.getCookies();
+				String cok_token = "nothing";
+
+				if (ck != null){
+				    for (int i = 0 ; i < ck.length ; i++){
+				      if (ck[i].getName().equals("TOKEN")){
+				        cok_token = ck[i].getValue();
+				      }
+				    }
+				  }
+
+				//ログイン認証
+				LoginBean login1 = new LoginBean((String)session.getAttribute("STATUS"),
+												(String)session.getAttribute("TOKEN"),
+												cok_token);
+				String result = login1.LoginCheck();
+
+				switch(result){
+				case "OK_ses": //ログインOK（セッションあり）
+					RequestDispatcher rd4 = getServletContext().getRequestDispatcher("/WEB-INF/mypage.jsp");
+					rd4.forward(req, resp);
+					break;
+				case "NG_ses": //ログインNG（セッションあり）
+					session.invalidate();                           //既存セッション破棄
+					RequestDispatcher rd1 = getServletContext().getRequestDispatcher("/WEB-INF/login.jsp");
+					rd1.forward(req, resp);
+					break;
+				case "OK_cok": //ログインOK（セッションなし）
+					session.invalidate();                           //既存セッション破棄
+					HttpSession newSession = req.getSession(true);  //新規セッション開始
+					newSession.setAttribute("TOKEN", cok_token);    //セッションに値を格納
+					newSession.setAttribute("STATUS", "login");     //セッションに値を格納
+
+					RequestDispatcher rd2 = getServletContext().getRequestDispatcher("/WEB-INF/mypage.jsp");
+					rd2.forward(req, resp);
+					break;
+				case "NG_cok": //ログインNG（セッションなし）
+					RequestDispatcher rd3 = getServletContext().getRequestDispatcher("/WEB-INF/login.jsp");
+					rd3.forward(req, resp);
+					break;
+				}
 
 			case "contact":
 				/*/WEB-INF/contact.jspを呼び出す */
